c6b6f8a4cab4e39542f0f7b2695741a659a7f2fe
Kyle Haywood <kyle@DESKTOP-KRSPB71.localdomain>
2022-04-26 20:11:13 -0400

    Additional bootstrap changes

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, TicketHistoryPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
+    path('ticket-history/', TicketHistoryPageView.as_view(), name='ticket-history')
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,6 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+class TicketHistoryPageView(TemplateView):
+    template_name = 'ticket-history.html'
