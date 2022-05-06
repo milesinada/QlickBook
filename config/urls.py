d117e6b120366232bf11e450d8519960fccdd06d
Miles Inada <milesinada219@gmail.com>
2022-05-06 00:46:42 -1000

    login page form, misc navigations

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("pages.urls")),
     path('projects/', include('projects.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
