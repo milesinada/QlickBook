ef250aebb62d08e3adfd603e7352251c7465a96e
Miles Inada <milesinada219@gmail.com>
2022-05-07 17:37:36 -1000

    Project New

--- projects/urls.py
+++ projects/urls.py
@@ -1,16 +1,9 @@
 from django.urls import path
-<<<<<<< HEAD
-from .views import DashboardPageView, ProjectListView, ProjectCreateView
-=======
-from .views import DashboardPageView, ProjectListView, ProjectDetailView
->>>>>>> 3bdaa7efadb13bae6e0bd5a32da000214d6e3ae3
+from .views import DashboardPageView, ProjectListView, ProjectDetailView, ProjectCreateView
 
 urlpatterns = [
     path('', DashboardPageView.as_view(), name='dashboard'),
     path('list/', ProjectListView.as_view(), name='project_list'),
-<<<<<<< HEAD
-    path('new/', ProjectCreateView.as_view(), name='project_new'),
-=======
     path('<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
->>>>>>> 3bdaa7efadb13bae6e0bd5a32da000214d6e3ae3
+    path('new/', ProjectCreateView.as_view(), name='project_new'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-from django.views.generic import TemplateView, ListView
-from django.contrib.auth.mixins import LoginRequiredMixin 
-from django.views.generic.edit import CreateView
-=======
 from django.views.generic import TemplateView, ListView, DetailView
->>>>>>> 3bdaa7efadb13bae6e0bd5a32da000214d6e3ae3
 from .models import Project
 
 # Create your views here.
@@ -15,13 +9,11 @@ class ProjectListView(ListView):
     template_name = 'projects/list.html'
     model = Project
 
-<<<<<<< HEAD
+class ProjectDetailView(DetailView):
+    template_name = 'projects/detail.html'
+    model = Project
+
 class ProjectCreateView(LoginRequiredMixin, CreateView):
     template_name = "projects/new.html"
     model = Project
     fields = ['title', 'description']
-=======
-class ProjectDetailView(DetailView):
-    template_name = 'projects/detail.html'
-    model = Project
->>>>>>> 3bdaa7efadb13bae6e0bd5a32da000214d6e3ae3
