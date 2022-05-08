d8c392571ae55951eaa314aad6b56a89c2c226f5
Kyle Haywood <kyle@DESKTOP-KRSPB71.localdomain>
2022-05-07 22:43:10 -0400

    URLs and Views for Accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render
 
 # Create your views here.
+class SignUpPageView(TemplateView):
+    template_name = 'registration/signup.html'
+
+class LoginPageView(TemplateView):
+    template_name = 'registration/login.html'
+
+class LoggedOutPageView(TemplateView):
+    template_name = 'registration/logged_out.html'
+
+class PasswordChangeFormPageView(TemplateView):
+    template_name = 'registration/password_change_form.html'
+
+class PasswordChangeDonePageView(TemplateView):
+    template_name = 'registration/password_change_done.html'
+
+class PasswordResetPageView(TemplateView):
+    template_name = 'registration/password_reset.html'
