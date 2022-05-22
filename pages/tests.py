11dcf40e838b424e67c180cb604fee04e4162507
Miles Inada <milesinada219@gmail.com>
2022-05-22 08:55:10 -1000

    Miless Mistake

--- pages/tests.py
+++ pages/tests.py
@@ -1,29 +1,29 @@
-# from django.test import SimpleTestCase
-# from django.urls import reverse
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# # Create your tests here.
-# class SimpleTests(SimpleTestCase):
-#     def test_home_page_status_code(self):
-#         response = self.client.get("/")
-#         self.assertEqual(response.status_code, 200)
+# Create your tests here.
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
 
-#     def test_about_page_status_code(self):
-#         response = self.client.get("/about/")
-#         self.assertEqual(response.status_code, 200)
+    def test_about_page_status_code(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
 
-#     def test_home_page_returns_correct_template(self):
-#         response = self.client.get("/")
-#         self.assertTemplateUsed(response, "index.html")
-#         self.assertTemplateUsed(response, "base.html")
+    def test_home_page_returns_correct_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "index.html")
+        self.assertTemplateUsed(response, "base.html")
 
-#     # def test_about_page_returns_correct_template(self):
-#     #     response = self.client.get("/about/")
-#     #     self.assertTemplateUsed(response, "about.html")
-#     #     self.assertTemplateUsed(response, "base.html")
+    def test_about_page_returns_correct_template(self):
+        response = self.client.get("/about/")
+        self.assertTemplateUsed(response, "about.html")
+        self.assertTemplateUsed(response, "icon-nav.html")
 
-#     def test_home_page_returns_correct_content(self):
-#         response = self.client.get("/")
-#         self.assertContains("Welcome to QlickBooks")
+    def test_home_page_returns_correct_content(self):
+        response = self.client.get("/")
+        self.assertContains(response, "Welcome to QlickBooks")
 
 # ##### ENABLE THIS WHEN CHECKING FOR CONTENT #########
 
@@ -33,10 +33,10 @@
 
 # #####################################################
 
-#     def test_home_page_reverse_lookup(self):
-#         response = self.client.get(reverse("home"))
-#         self.assertEqual(response.status_code, 200)
+    def test_home_page_reverse_lookup(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
 
-#     def test_about_page_reverse_lookup(self):
-#         response = self.client.get(reverse("about"))
-#         self.assertEqual(response.status_code, 200)
+    def test_about_page_reverse_lookup(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
