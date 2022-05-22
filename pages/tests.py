109b184f01551bbbb9524988039dfcbb1f84f16b
Kyle Haywood <kyle@DESKTOP-KRSPB71.localdomain>
2022-05-22 14:14:18 -0400

    Test Cases

--- pages/tests.py
+++ pages/tests.py
@@ -1,20 +1,20 @@
-# from django.test import SimpleTestCase
-# from django.urls import reverse
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# # Create your tests here.
-# class SimpleTests(SimpleTestCase):
-#     def test_home_page_status_code(self):
-#         response = self.client.get("/")
-#         self.assertEqual(response.status_code, 200)
+# Create your tests here.
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
 
-#     def test_about_page_status_code(self):
-#         response = self.client.get("/about/")
-#         self.assertEqual(response.status_code, 200)
+    def test_about_page_status_code(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
 
-#     def test_home_page_returns_correct_template(self):
-#         response = self.client.get("/")
-#         self.assertTemplateUsed(response, "index.html")
-#         self.assertTemplateUsed(response, "base.html")
+    def test_home_page_returns_correct_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "index.html")
+        self.assertTemplateUsed(response, "base.html")
 
     def test_about_page_returns_correct_template(self):
         response = self.client.get("/about/")
@@ -33,10 +33,10 @@
 
 # #####################################################
 
-#     def test_home_page_reverse_lookup(self):
-#         response = self.client.get(reverse("home"))
-#         self.assertEqual(response.status_code, 200)
+    def test_home_page_reverse_lookup(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
 
-#     def test_about_page_reverse_lookup(self):
-#         response = self.client.get(reverse("about"))
-#         self.assertEqual(response.status_code, 200)
+    def test_about_page_reverse_lookup(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
