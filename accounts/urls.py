58898d19c46f3780ef296e2e9648fac478dd7e4c
Miles Inada <milesinada219@gmail.com>
2022-05-26 16:58:43 -1000

    Creating relation Project and Ticket

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import SignUpPageView, LoginPageView, LoggedOutPageView, PasswordChangeFormPageView, PasswordChangeDonePageView, PasswordResetPageView
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('signup/', SignUpView.as_view(), name='signup'),    
-    path('logged_out/', LoggedOutPageView.as_view(), name='logged_out')
+    path('login/', LoginPageView.as_view(), name='login'),
+    path('signup/', SignUpPageView.as_view(), name='signup'),    
+    path('logged_out/', LoggedOutPageView.as_view(), name='logged_out'),
     path('password_change/', PasswordChangeFormPageView.as_view(), name='password_change'),
-    path('password_change_done/', PasswordChangeDonePageView.as_view(), name='password_change_done')
+    path('password_change_done/', PasswordChangeDonePageView.as_view(), name='password_change_done'),
     path('password_reset/', PasswordResetPageView.as_view(), name='password_reset'),
 
 ]
