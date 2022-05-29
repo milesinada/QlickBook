30ec4e63a8d92e90008287d47db905cdd879aba7
Ray Garcia <raymarco1023@gmail.com>
2022-05-28 18:19:05 -0700

    ray's code

--- accounts/models.py
+++ accounts/models.py
@@ -19,3 +19,25 @@
 #         blank=True
 #     )
 #     is_active = models.BooleanField()
+
+
+from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser
+                                        )
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+    active = models.BooleanField(default=True)  # can login
+    staff = models.BooleanField(default=False)  # staff user non superuser
+    admin = models.BooleanField(default=False)  # superuser
+
+    USERNAME_FIELD = 'email'  # username
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
