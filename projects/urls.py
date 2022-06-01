8ed4f5479c68d4a0dd53dac9331e82e3c8d75262
Miles Inada <milesinada219@gmail.com>
2022-05-31 14:31:54 -1000

    Solved Ticket Model Relation

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 
-from projects.views import TicketListView
 from .views import (
     DashboardPageView,
     ProjectListView, 
@@ -8,13 +7,21 @@ from .views import (
     ProjectCreateView, 
     TicketCreateView,
     TicketListView,
+    TicketDeleteView,
+    TicketDetailView,
+    TicketUpdateView
 )
 
 urlpatterns = [
     path('', DashboardPageView.as_view(), name='dashboard'),
     path('list/', ProjectListView.as_view(), name='project_list'),
-    path('<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
+    path('<int:pk>/',
+        ProjectDetailView.as_view(), name='project_detail'),
     path('new-project/', ProjectCreateView.as_view(), name='project_new'),
     path('new-ticket/', TicketCreateView.as_view(), name='ticket_new'),
     path('tickets/', TicketListView.as_view(), name='ticket_list'),
+    path('<int:pk>/ticket/', TicketDetailView.as_view(), name='ticket_detail'),
+    path('<int:pk>/edit-ticket/', TicketUpdateView.as_view(), name='ticket_edit'),
+    path('<int:pk>/delete-ticket/', TicketDeleteView.as_view(), name='ticket_delete'),
+
 ]
