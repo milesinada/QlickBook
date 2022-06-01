efcf1a1aa263ca17d08d37bdb3c2089298777868
Kyle Haywood <kyle@DESKTOP-KRSPB71.localdomain>
2022-05-31 22:33:16 -0400

    Git conflict issues

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView, ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin 
-from django.views.generic.edit import CreateView
-
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
 from .models import Project, Ticket
 
 # Create your views here.
@@ -13,23 +13,42 @@ class ProjectListView(ListView):
     model = Project
 
 class ProjectDetailView(DetailView):
-    template_name = 'projects/detail.html'
     model = Project
-    queryset = Project.objects.all()
-    ticket_queryset = Ticket.objects.all()
-    ticket_list = ticket_queryset.select_related('Project')
+    template_name = 'projects/detail.html'
+    def get_queryset(self):
+        return Project.objects.filter(id=self.kwargs['pk'])
     
+    def get_context_data(self, **kwargs):
+        context = super(ProjectDetailView, self).get_context_data(**kwargs)
+        context["ticket_list"] = Ticket.objects.filter(project_id=self.kwargs['pk'])
+        return context
+        # select_related used if only need 1 query to server. would be better to use in ticket_detail rather Project detail. In Ticket_detail to show Project.title use S_R()
 
 class ProjectCreateView(LoginRequiredMixin, CreateView):
     template_name = "projects/new.html"
     model = Project
     fields = ['title', 'description']
 
+class TicketListView(ListView):
+    template_name = 'tickets/ticket-list.html'
+    model = Ticket
+
 class TicketCreateView(LoginRequiredMixin, CreateView):
     template_name = "tickets/new.html"
     model = Ticket
     fields = ['title', 'author', 'dateCreated', 'dateResolved', 'difficulty', 'status', 'project', 'commentary']
 
-class TicketListView(ListView):
-    template_name = 'tickets/ticket-list.html'
+class TicketDetailView(DetailView):
+    template_name = "tickets/detail.html"
     model = Ticket
+
+class TicketUpdateView(UpdateView):
+    template_name = "tickets/edit.html"
+    model = Ticket
+    fields = ['title', 'author', 'dateCreated', 'dateResolved', 'difficulty', 'status', 'project', 'commentary']
+
+class TicketDeleteView(DeleteView):
+    template_name = "tickets/delete.html"
+    model = Ticket
+    success_url = reverse_lazy('project_list')
+
