b7bfd67e5b347a672d63e4ad6a53e705fcdcf865
Kyle Haywood <kyle@DESKTOP-KRSPB71.localdomain>
2022-06-02 20:44:12 -0400

    nothing added

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,7 +5,7 @@
 # admin.site.register(Status)
 
 from django.contrib import admin
-from django.contrib import get_user_model
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
