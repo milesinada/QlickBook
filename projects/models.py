becd615a2152384128112d32c240eb0ac95ffefe
Miles Inada <milesinada219@gmail.com>
2022-06-10 11:55:54 -1000

    navagation bug fixes

--- projects/models.py
+++ projects/models.py
@@ -31,8 +31,6 @@ class Ticket(models.Model):
         PROGRESS = 1, ('In Progress')
         COMPLETED = 2, ('Completed')
 
-        def __str__(self):
-            return self
 
     author = models.ForeignKey(
         get_user_model(),
