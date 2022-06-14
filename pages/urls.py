66182fe0293591bd2e03e253b379b89be022f5e6
Miles Inada <milesinada219@gmail.com>
2022-06-14 12:59:26 -1000

    Bootstrap Icons

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, TicketHistoryPageView, FaqPageView
+from .views import HomePageView, AboutPageView, TicketHistoryPageView, FaqPageView, ContactPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('ticket-history/', TicketHistoryPageView.as_view(), name='ticket-history'),
     path('faq/', FaqPageView.as_view(), name='faq'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
 
 ]
