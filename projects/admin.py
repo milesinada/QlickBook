b552d4a7c7956248ff0bc138c157b8733608fd4c
Miles Inada <milesinada219@gmail.com>
2022-06-16 18:02:27 -1000

    sprints cleaned

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Project, Ticket
+from .models import Project, Ticket, Sprint
 
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(Ticket)
+admin.site.register(Sprint)
