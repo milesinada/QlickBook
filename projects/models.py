cb5855c866ea80aa5c1cf7fdfe2883e2defc3f72
Miles Inada <milesinada219@gmail.com>
2022-06-23 17:55:58 -1000

    sprint details, added milestones

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from multiprocessing import AuthenticationError
 from time import timezone
 from django.db import models 
@@ -23,6 +24,13 @@ class Project(models.Model):
 
 
 class Sprint(models.Model):
+
+    class Milestone(models.IntegerChoices):
+        FIRST = 1, ('First')
+        SECOND = 2, ('Second')
+        THIRD = 3, ('Third')
+        FINAL = 4, ('Final')
+
     LENGTH_CHOICES = (('1_week', '1 Week'), ('2_weeks', '2 Weeks'), ('3_weeks', '3 Weeks'))
     title = models.CharField(max_length=200)
     created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="sprints", null=True)
@@ -32,15 +40,18 @@ class Sprint(models.Model):
     duration = models.CharField(max_length = 10, choices = LENGTH_CHOICES)
     complete = models.BooleanField(null=True, default=False)
     retrospective = models.CharField(max_length=10000, blank=True, null=True)
+    milestone = models.IntegerField(default=Milestone.FIRST, choices=Milestone.choices)
 
     def __str__(self):
         return self.title
 
 class Ticket(models.Model):
     class DifficultyLevel(models.IntegerChoices):
-        EASY = 0, ('Easy')
-        NORMAL = 1, ('Normal')
-        HARD = 2, ('HARD')
+        ENTRY = 0, ('Entry')
+        JUNIOR = 1, ('Junior')
+        MID = 2, ('Mid')
+        SENIOR = 3, ('Senior')
+        LEAD = 4, ('Lead')
     class StatusLevel(models.IntegerChoices):
         NOTSTARTED = 0, ('Not Started')
         PROGRESS = 1, ('In Progress')
@@ -49,14 +60,17 @@ class Ticket(models.Model):
     author = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
     dateCreated = models.DateTimeField(default=timezone.now)
     dateResolved = models.DateTimeField(blank = True, null=True)
-    difficulty = models.IntegerField(default=DifficultyLevel.EASY, choices=DifficultyLevel.choices)
+    milestone_resolved = models.IntegerField(default=0)
+    difficulty = models.IntegerField(default=DifficultyLevel.ENTRY, choices=DifficultyLevel.choices)
     status = models.IntegerField(default=StatusLevel.NOTSTARTED, choices=StatusLevel.choices)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    sprint = models.ForeignKey(Sprint, on_delete=models.CASCADE)
+    sprint = models.ForeignKey(Sprint, on_delete=models.CASCADE, blank=True, null=True)
     commentary = models.TextField(max_length=256)
 
     assigned_to = models.ManyToManyField(get_user_model(), related_name='assigned_users')
     
+    #if complete status(2) is updated, dateResolved should be updated
+
     def __str__(self):
         return self.title
 
